8674784bbb351a0f9877c2560a3afe5b55ad2784
SHandl19 <stefan@stefanhandl.com>
2025-01-22 13:27:19 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Pluginwoo
+# WooCommerce Spezial-Produkt Plugin
+
+## 📌 Funktionen:
+- Registriert eine neue Produktart **"Spezial-Produkt"**
+- Automatische Varianten basierend auf Größen (Kinder/Erwachsene)
+- Speichert Preis & Lagerbestand korrekt
+- Swatches statt Dropdowns im Frontend
+- Dynamische Preis- und Bestandsaktualisierung
+
+## 🚀 Installation:
+1. Lade die Dateien in `/wp-content/plugins/woocommerce-special-product/`
+2. Aktiviere das Plugin in WordPress
+3. Erstelle ein neues Produkt und wähle **"Spezial-Produkt"**
+
+## 📌 Anforderungen:
+- WordPress 6.0+
+- WooCommerce 9.6.0+
+- jQuery (WooCommerce-Standard)
---
+++ woocommerce-special-product.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Spezial-Produkt
+ * Plugin URI:  https://deine-webseite.de
+ * Description: Fügt eine neue WooCommerce-Produktart mit individuellen Varianten & Swatches hinzu.
+ * Version: 1.0.0
+ * Author: Dein Name
+ * License: GPL v2 or later
+ */
+
+if (!defined('ABSPATH')) exit;
+
+// Autoload Klassen
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-product.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-tab.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-ajax.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-swatches.php';
+require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-stock.php';
+/*require_once plugin_dir_path(__FILE__) . 'includes/class-wc-special-price.php';*/
+
+// Plugin aktivieren
+function wc_special_product_activate() {
+    if (!class_exists('WooCommerce')) {
+        wp_die('WooCommerce muss installiert und aktiviert sein.');
+    }
+}
+register_activation_hook(__FILE__, 'wc_special_product_activate');
+function enqueue_admin_scripts() {
+    wp_enqueue_script('special-product-admin', plugin_dir_url(__FILE__) . 'assets/admin.js', ['jquery'], null, true);
+}
+add_action('admin_enqueue_scripts', 'enqueue_admin_scripts');
